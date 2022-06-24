72cc492961853e4df987cceb53af9173ab911a29
asaduzzaman69 <=>
2022-06-24 21:01:58 +0600

    created navbar

---
+++ src/components/Inventories/Inventories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventories = () => {
+    return (
+        <div>
+            <h5>Inventory</h5>
+        </div>
+    );
+};
+
+export default Inventories;
---
+++ src/components/Signup/Signup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Signup = () => {
+    return (
+        <div>
+            <h3>Sing </h3>
+        </div>
+    );
+};
+
+export default Signup;
